906794d3500af1a8ef43e1aded65763a56cd06af
foxalan <33643467+foxalan@users.noreply.github.com>
2018-06-24 11:22:47 +0800

    create bean user and userfollow

--- src/main/java/net/qiujuer/web/italker/push/bean/db/User.java
+++ src/main/java/net/qiujuer/web/italker/push/bean/db/User.java
@@ -1,10 +1,16 @@
 package net.qiujuer.web.italker.push.bean.db;
 
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
+/**
+ * 用户表
+ */
 @Table(name = "tb_user")
 @Entity
 public class User {
@@ -20,9 +26,145 @@ public class User {
     @Column(updatable = false,nullable = false)
     private String id;
 
+    //用户名唯一
+    @Column(nullable = false,length = 128,unique = true)
+    private String name;
 
+    //电话号唯一
+    @Column(nullable = false,length = 62,unique = true)
+    private String phone;
 
+    //密码
+    @Column(nullable = false)
+    private String password;
 
+    //头像
+    @Column
+    private String portrait;
 
+    //描述
+    @Column
+    private String description;
 
+    //性别
+    @Column(nullable = false)
+    private int sex = 0;
+
+    //可以拉取用户信息，并且唯一
+    @Column(unique = true)
+    private String token;
+
+    //用于推送的设备ID
+    @Column
+    private String pushId;
+
+    //创建时间
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createAt = LocalDateTime.now();
+
+    //更新时间
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updateAt = LocalDateTime.now();
+
+    //最后一次
+    @Column
+    private LocalDateTime lastReceivedAt = LocalDateTime.now();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPortrait() {
+        return portrait;
+    }
+
+    public void setPortrait(String portrait) {
+        this.portrait = portrait;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getPushId() {
+        return pushId;
+    }
+
+    public void setPushId(String pushId) {
+        this.pushId = pushId;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public LocalDateTime getLastReceivedAt() {
+        return lastReceivedAt;
+    }
+
+    public void setLastReceivedAt(LocalDateTime lastReceivedAt) {
+        this.lastReceivedAt = lastReceivedAt;
+    }
 }
---
+++ src/main/java/net/qiujuer/web/italker/push/bean/db/UserFollow.java
@@ -0,0 +1,162 @@
+package net.qiujuer.web.italker.push.bean.db;
+
+
+import org.hibernate.annotations.*;
+
+import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 用户关注表
+ */
+
+@Table(name = "tb_user")
+@Entity
+public class UserFollow {
+
+    @Id
+    //主键
+    @PrimaryKeyJoinColumn
+    //一种基本没有重复值的ID
+    @GeneratedValue(generator = "uuid")
+    //把uuid的生成器定义为uuid2,uuid2是常规的UUID toString
+    @GenericGenerator(name = "uuid",strategy = "uuid2")
+    //不允许更改，不允许为null
+    @Column(updatable = false,nullable = false)
+    private String id;
+
+
+    //关注人
+    //多对一，你可以关注很多人
+    //optional不可以选，必须存储
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "originId")
+    private User origin;
+
+    @Column(nullable = false,updatable = false,insertable = false)
+    private String originId;
+
+    //被关注者
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "targetId")
+    private User target;
+
+    @Column(nullable = false,updatable = false,insertable = false)
+    private String targetId;
+
+    //备注
+    @Column
+    private String alias;
+
+    //创建时间
+    @CreationTimestamp
+    @Column
+    private LocalDateTime createAt = LocalDateTime.now();
+
+    //更新时间
+    @UpdateTimestamp
+    @Column
+    private LocalDateTime updateAt = LocalDateTime.now();
+
+    //我关注的人的列表方法
+    //对应的数据库的字段为TB_USER_FOLLOW.originId
+    @JoinColumn(nullable = "originId")
+    //定义为懒加载，默认加载User信息的时候，并不查询这个集合
+    @LazyCollection(LazyCollectionOption.EXTRA)
+    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    private Set<UserFollow> following = new HashSet<>();
+
+    //关注我的人
+    @JoinColumn(nullable = "targetId")
+    //定义为懒加载，默认加载User信息的时候，并不查询这个集合
+    @LazyCollection(LazyCollectionOption.EXTRA)
+    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    private Set<UserFollow> follows = new HashSet<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public Set<UserFollow> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(Set<UserFollow> following) {
+        this.following = following;
+    }
+
+    public Set<UserFollow> getFollows() {
+        return follows;
+    }
+
+    public void setFollows(Set<UserFollow> follows) {
+        this.follows = follows;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public User getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(User origin) {
+        this.origin = origin;
+    }
+
+    public String getOriginId() {
+        return originId;
+    }
+
+    public void setOriginId(String originId) {
+        this.originId = originId;
+    }
+
+    public User getTarget() {
+        return target;
+    }
+
+    public void setTarget(User target) {
+        this.target = target;
+    }
+
+    public String getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(String targetId) {
+        this.targetId = targetId;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    //
+}
