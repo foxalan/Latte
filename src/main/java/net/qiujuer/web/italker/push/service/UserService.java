c8c641216f9400b38386cf719dd0a0882d9d6482
foxalan <33643467+foxalan@users.noreply.github.com>
2018-07-23 14:29:24 +0800

    add all service

--- src/main/java/net/qiujuer/web/italker/push/service/UserService.java
+++ src/main/java/net/qiujuer/web/italker/push/service/UserService.java
@@ -1,16 +1,18 @@
 package net.qiujuer.web.italker.push.service;
 
+import com.google.common.base.Strings;
 import net.qiujuer.web.italker.push.bean.api.base.ResponseModel;
 import net.qiujuer.web.italker.push.bean.api.user.UpdateInfoModel;
 import net.qiujuer.web.italker.push.bean.card.UserCard;
 import net.qiujuer.web.italker.push.bean.db.User;
+import net.qiujuer.web.italker.push.bean.db.UserFollow;
 import net.qiujuer.web.italker.push.factory.UserFactory;
+import net.qiujuer.web.italker.push.utils.Hib;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/user")
 public class UserService extends BaseService {
@@ -35,4 +37,179 @@ public class UserService extends BaseService {
         // 返回
         return ResponseModel.buildOk(card);
     }
+
+
+    // 搜索人的接口实现
+    // 为了简化分页：只返回20条数据
+    @GET // 搜索人，不涉及数据更改，只是查询，则为GET
+    // http://127.0.0.1/api/user/search/
+    @Path("/search/{name:(.*)?}") // 名字为任意字符，可以为空
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseModel<List<UserCard>> search(@DefaultValue("") @PathParam("name") String name) {
+        User self = getSelf();
+
+        System.out.println("search name:"+name);
+        // 先查询数据
+        List<User> searchUsers = UserFactory.search(name);
+        System.out.println("search name:"+searchUsers.size());
+        // 把查询的人封装为UserCard
+        // 判断这些人是否有我已经关注的人，
+        // 如果有，则返回的关注状态中应该已经设置好状态
+        System.out.println("search name item:");
+        // 拿出我的联系人
+        final List<User> contacts = UserFactory.contacts(self);
+
+        // 把User->UserCard
+        List<UserCard> userCards = searchUsers.stream()
+                .map(user -> {
+                    // 判断这个人是否是我自己，或者是我的联系人中的人
+                    boolean isFollow = user.getId().equalsIgnoreCase(self.getId())
+                            // 进行联系人的任意匹配，匹配其中的Id字段
+                            || contacts.stream().anyMatch(
+                            contactUser -> contactUser.getId()
+                                    .equalsIgnoreCase(user.getId())
+                    );
+
+                    return new UserCard(user, isFollow);
+                }).collect(Collectors.toList());
+        // 返回
+        return ResponseModel.buildOk(userCards);
+    }
+
+    // 获取某人的信息
+    @GET
+    @Path("{id}") // http://127.0.0.1/api/user/{id}
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseModel<UserCard> getUser(@PathParam("id") String id) {
+        if (Strings.isNullOrEmpty(id)) {
+            // 返回参数异常
+            return ResponseModel.buildParameterError();
+        }
+
+
+        User self = getSelf();
+        if (self.getId().equalsIgnoreCase(id)) {
+            // 返回自己，不必查询数据库
+            return ResponseModel.buildOk(new UserCard(self, true));
+        }
+
+
+        User user = UserFactory.findById(id);
+        if (user == null) {
+            // 没找到，返回没找到用户
+            return ResponseModel.buildNotFoundUserError(null);
+        }
+
+
+        // 如果我们直接有关注的记录，则我已关注需要查询信息的用户
+        boolean isFollow = UserFactory.getUserFollow(self, user) != null;
+        return ResponseModel.buildOk(new UserCard(user, isFollow));
+    }
+
+
+
+    /**
+     * 关注人的操作
+     *
+     * @param origin 发起者
+     * @param target 被关注的人
+     * @param alias  备注名
+     * @return 被关注的人的信息
+     */
+    public static User follow(final User origin, final User target, final String alias) {
+        UserFollow follow = getUserFollow(origin, target);
+        if (follow != null) {
+            // 已关注，直接返回
+            return follow.getTarget();
+        }
+
+        return Hib.query(session -> {
+            // 想要操作懒加载的数据，需要重新load一次
+            session.load(origin, origin.getId());
+            session.load(target, target.getId());
+
+            // 我关注人的时候，同时他也关注我，
+            // 所有需要添加两条UserFollow数据
+            UserFollow originFollow = new UserFollow();
+            originFollow.setOrigin(origin);
+            originFollow.setTarget(target);
+            // 备注是我对他的备注，他对我默认没有备注
+            originFollow.setAlias(alias);
+
+            // 发起者是他，我是被关注的人的记录
+            UserFollow targetFollow = new UserFollow();
+            targetFollow.setOrigin(target);
+            targetFollow.setTarget(origin);
+
+            // 保存数据库
+            session.save(originFollow);
+            session.save(targetFollow);
+
+            return target;
+        });
+    }
+
+    /**
+     * 查询两个人是否已经关注
+     *
+     * @param origin 发起者
+     * @param target 被关注人
+     * @return 返回中间类UserFollow
+     */
+    public static UserFollow getUserFollow(final User origin, final User target) {
+        return Hib.query(session -> (UserFollow) session
+                .createQuery("from UserFollow where originId = :originId and targetId = :targetId")
+                .setParameter("originId", origin.getId())
+                .setParameter("targetId", target.getId())
+                .setMaxResults(1)
+                // 唯一查询返回
+                .uniqueResult());
+    }
+
+
+    // 关注人，
+    // 简化：关注人的操作其实是双方同时关注
+    @PUT // 修改类使用Put
+    @Path("/follow/{followId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseModel<UserCard> follow(@PathParam("followId") String followId) {
+        User self = getSelf();
+
+        // 不能关注我自己
+        if (self.getId().equalsIgnoreCase(followId)
+                || Strings.isNullOrEmpty(followId)) {
+            // 返回参数异常
+            return ResponseModel.buildParameterError();
+        }
+
+
+        // 找到我也关注的人
+        User followUser = UserFactory.findById(followId);
+        if (followUser == null) {
+            // 未找到人
+            return ResponseModel.buildNotFoundUserError(null);
+        }
+
+        // 备注默认没有，后面可以扩展
+        followUser = UserFactory.follow(self, followUser, null);
+        if (followUser == null) {
+            // 关注失败，返回服务器异常
+            return ResponseModel.buildServiceError();
+        }
+
+        // TODO 通知我关注的人我关注他
+
+        // 返回关注的人的信息
+        return ResponseModel.buildOk(new UserCard(followUser, true));
+    }
+
+
+
+
+
+
+
 }
